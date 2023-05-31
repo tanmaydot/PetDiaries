ed1d5552163a5dc709a58aa6bac256d311b3c0e9
tanmaydot <reachtanmays@gmail.com>
2023-05-31 18:50:17 +0530

    renderlinkupdate

--- MAIN/client/src/scenes/widgets/PostWidget.jsx
+++ MAIN/client/src/scenes/widgets/PostWidget.jsx
@@ -38,14 +38,17 @@ const PostWidget = ({
   const primary = palette.primary.main;
 
   const patchLike = async () => {
-    const response = await fetch(`http://localhost:3001/posts/${postId}/like`, {
-      method: "PATCH",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ userId: loggedInUserId }),
-    });
+    const response = await fetch(
+      `https://pet-diaries-server.onrender.com/posts/${postId}/like`,
+      {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userId: loggedInUserId }),
+      }
+    );
     const updatedPost = await response.json();
     dispatch(setPost({ post: updatedPost }));
   };
@@ -55,7 +58,7 @@ const PostWidget = ({
       return; // do not allow deletion if the user is not the owner of the post
     }
     const response = await fetch(
-      `http://localhost:3001/assets/${picturePath}`,
+      `https://pet-diaries-server.onrender.com/assets/${picturePath}`,
       {
         method: "DELETE",
         headers: {
@@ -88,7 +91,7 @@ const PostWidget = ({
           height="auto"
           alt="post"
           style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
-          src={`http://localhost:3001/assets/${picturePath}`}
+          src={`https://pet-diaries-server.onrender.com/assets/${picturePath}`}
         />
       )}
       <FlexBetween mt="0.25rem">
